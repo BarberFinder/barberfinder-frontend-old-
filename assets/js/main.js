f6a2cd70650116c92e69c3fad973ee7341fb0cd6
Hansin Susatya <hansinsusatya@gmail.com>
2018-09-13 14:09:58 +0700

    Fixing carousel when user switch route

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,91 +25,12 @@
 			}
 		});
 	});
-	/*=========================================================================
-         Main Slider
-=========================================================================*/
-	$(document).ready(function() {
-		$('#main-slider').owlCarousel({
-			loop: true,
-			autoplay: true,
-			smartSpeed: 500,
-			items: 1,
-			nav: true,
-			navText: [ '<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>' ]
-		});
-		$('#main-slider').on('translate.owl.carousel', function() {
-			$('.main_slide .slider_content h3').removeClass('animated fadeInUp').css('opacity', '0');
-			$('.main_slide .slider_content h1').removeClass('animated fadeInUp').css('opacity', '0');
-			$('.main_slide .slider_content p, .main_slide .slider_content .btn_group')
-				.removeClass('animated fadeInUp')
-				.css('opacity', '0');
-		});
-
-		$('#main-slider').on('translated.owl.carousel', function() {
-			$('.main_slide .slider_content h3').addClass('animated fadeInUp').css('opacity', '1');
-			$('.main_slide .slider_content h1').addClass('animated fadeInUp').css('opacity', '1');
-			$('.main_slide .slider_content p, .main_slide .slider_content .btn_group')
-				.addClass('animated fadeInUp')
-				.css('opacity', '1');
-		});
-	});
-
-	/*=========================================================================
-    Gallery Slider
-=========================================================================*/
-
-	$('#gallery-slide').owlCarousel({
-		loop: true,
-		autoplay: true,
-		smartSpeed: 500,
-		items: 1,
-		dots: false,
-		nav: true,
-		navText: [ '<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>' ]
-	});
 
 	/*=========================================================================
         Initialize smoothscroll plugin
 =========================================================================*/
 	smoothScroll.init({
 		offset: 60
-	});
-
-	/*=========================================================================
-        Testimonial Carousel
-=========================================================================*/
-	$('#testimonial_carousel').owlCarousel({
-		loop: true,
-		autoplay: true,
-		smartSpeed: 500,
-		items: 1,
-		nav: false
-	});
-
-	/*=========================================================================
-    Food Carousel
-=========================================================================*/
-	$('#food_carousel').imagesLoaded(function() {
-		$('#food_carousel').owlCarousel({
-			loop: true,
-			margin: 10,
-			autoplay: true,
-			smartSpeed: 500,
-			nav: false,
-			dots: false,
-			responsive: true,
-			responsive: {
-				0: {
-					items: 1
-				},
-				480: {
-					items: 3
-				},
-				768: {
-					items: 4
-				}
-			}
-		});
 	});
 
 	/*=========================================================================
