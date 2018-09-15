c29d7cc8d78674b64750e573b1669ba3e0a4a1eb
mnoorcahyo <mnoorcahyo@gmail.com>
2018-09-15 17:17:13 +0700

    edit signup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
-=======
-import 'semantic-ui-css/semantic.min.css';
-import App from './App';
->>>>>>> master
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -19,46 +19,139 @@ import {
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class SignUp extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      user: [{}],
+      input_username: "",
+      input_password: "",
+      input_firstname: "",
+      input_lastname: "",
+      input_email: "",
+      input_phone: ""
+    };
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addUser = () => {
+    let prevUser = this.state.user.slice();
+    prevUser.push({
+      username: this.state.input_username,
+      password: this.state.input_password,
+      firstname: this.state.input_firstname,
+      lastname: this.state.input_lastname,
+      email: this.state.input_email,
+      phone: this.state.input_phone
+    });
+  };
+
   render() {
     return (
       <div>
         <Link to="/signup">
           <Container>
-            <Header as="h1" className="headerLogin">
+            <Header as="h1" className="headerSignup">
               Sign Up
             </Header>
-            <Header as="h3" className="headerLogin2">
-              Already have a Wix account? <Link to="/login"> <a href="#signup">Log In</a> </Link>
+            <Header as="h3" className="headerSignup2">
+              Already have a Wix account?{" "}
+              <Link to="/login">
+                {" "}
+                <a href="#signup">Log In</a>{" "}
+              </Link>
             </Header>
             <Grid columns={2} padded>
               <Grid.Column>
-                <Form className="form1loginpage">
+                <Form className="form1Signuppage">
+                  <Grid columns={2} padded>
+                    <Grid.Column className="firstnameSU">
+                      <Form.Field>
+                        <Input
+                          transparent
+                          placeholder="First Name"
+                          type="text"
+                          name="input_firstname"
+                          value={this.state.input_firstname}
+                          onChange={this.handleOnChange}
+                        />
+                        <Divider />
+                      </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column className="lastnameSU">
+                      <Form.Field>
+                        <Input
+                          transparent
+                          placeholder="Last Name"
+                          type="text"
+                          name="input_lastname"
+                          value={this.state.input_lastname}
+                          onChange={this.handleOnChange}
+                        />
+                        <Divider />
+                      </Form.Field>
+                    </Grid.Column>
+                  </Grid>
+
                   <Form.Field>
-                    <Input transparent placeholder="Email" />
+                    <Input
+                      transparent
+                      placeholder="Username"
+                      type="text"
+                      name="input_username"
+                      value={this.state.input_username}
+                      onChange={this.handleOnChange}
+                    />
                     <Divider />
                   </Form.Field>
 
                   <Form.Field>
-                    <Input transparent placeholder="Type your email again" />
+                    <Input
+                      transparent
+                      placeholder="Password"
+                      type="password"
+                      name="input_password"
+                      value={this.state.input_password}
+                      onChange={this.handleOnChange}
+                    />
                     <Divider />
                   </Form.Field>
 
                   <Form.Field>
-                    <Input transparent placeholder="Password" />
+                    <Input
+                      transparent
+                      placeholder="Email"
+                      type="email"
+                      name="input_email"
+                      value={this.state.input_email}
+                      onChange={this.handleOnChange}
+                    />
                     <Divider />
                   </Form.Field>
 
                   <Form.Field>
-                    <Input transparent placeholder="Type your password again" />
+                    <Input
+                      transparent
+                      placeholder="Phone"
+                      type="text"
+                      name="input_phone"
+                      value={this.state.input_phone}
+                      onChange={this.handleOnChange}
+                    />
                     <Divider />
                   </Form.Field>
                   <Container textAlign="left">
-                    <Button inverted color='blue' className="btonsignup">Sign Up</Button>
+                    <Button inverted color="blue" className="btonsignup">
+                      Sign Up
+                    </Button>
                   </Container>
                 </Form>
               </Grid.Column>
-              <Grid.Column className="gridCol2login form2loginpage">
+              <Grid.Column className="gridCol2Signup form2Signuppage">
                 <Button as="div" labelPosition="left">
                   <Label as="a" className="logobton" basic>
                     <img src={Logofb} />
